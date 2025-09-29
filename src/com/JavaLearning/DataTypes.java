e166954c7c1febbc795c0d4890ad1cba8f3a3e22
Aryan <bhardwaj7a@gmail.com>
2025-09-29 15:05:52 +0530

    Integral Numbers In Java

---
+++ src/com/JavaLearning/DataTypes.java
@@ -0,0 +1,41 @@
+package com.JavaLearning;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive Data Types
+
+        // Integral numbers
+         //byte
+         //short
+         //int
+         //long
+        // decimal numbers
+         // float
+         // double
+        // characters
+         // char
+         // booleans
+         // bool
+
+        byte age1 = 20;
+        short age2 = 20;
+        int age = 20;
+        long age3 = 20;
+
+        System.out.println("Byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        System.out.println("Short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        System.out.println("Integer");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println("Long");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+    }
+}
