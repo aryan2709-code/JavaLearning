50abcf578e548ac92b65d25f39a3cafe4aca31c3
Aryan <bhardwaj7a@gmail.com>
2025-09-29 15:20:56 +0530

    Decimal numbers in Java

--- src/com/JavaLearning/DataTypes.java
+++ src/com/JavaLearning/DataTypes.java
@@ -17,25 +17,58 @@ public class DataTypes {
          // booleans
          // bool
 
+        // Integral Numbers
         byte age1 = 20;
         short age2 = 20;
         int age = 20;
         long age3 = 20;
 
-        System.out.println("Byte");
-        System.out.println(Byte.MIN_VALUE);
-        System.out.println(Byte.MAX_VALUE);
+//        System.out.println("Byte");
+//        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+//
+//        System.out.println("Short");
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Short.MAX_VALUE);
+//
+//        System.out.println("Integer");
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//
+//        System.out.println("Long");
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
 
-        System.out.println("Short");
-        System.out.println(Short.MIN_VALUE);
-        System.out.println(Short.MAX_VALUE);
+        // Decimal Numbers In Java
+        float mySalary = 1000.111123112121f; // f is used to communicate to java that it is a float number
+        double yourSalary = 1000.11112311212121;
+        System.out.println(mySalary);
+        System.out.println(yourSalary);
 
-        System.out.println("Integer");
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
+        // float has approx 7 decimal digits of precision
+        // double has approx 15 decimal digits of precision
 
-        System.out.println("Long");
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Long.MIN_VALUE);
+        mySalary =   105555555555555555555555555555500.11112311112121f;
+        yourSalary = 105555555555555555555555555555500.11112311112121;
+
+        System.out.println(mySalary);
+        System.out.println(yourSalary);
+
+        // The above two print statements give a number in scientific notation as o/p.
+
+        // Float range
+        float floatMin = Float.MIN_VALUE;
+        float floatMax = Float.MAX_VALUE;
+
+        System.out.println("Float Minimum Value: " + floatMin);
+        System.out.println("Float Maximum Value: " + floatMax);
+
+        // Double range
+        double doubleMin = Double.MIN_VALUE;
+        double doubleMax = Double.MAX_VALUE;
+
+        System.out.println("Double Minimum Value: " + doubleMin);
+        System.out.println("Double Maximum Value: " + doubleMax);
+
     }
 }
